51cff1685d4dc3c2ef4bf128c042c975dddc7c45
Kevin <kevin@gmail.com>
2021-02-15 20:57:46 +0800

    Index開發中

---
+++ MemberManagementSystem/Controllers/MemberController.cs
@@ -0,0 +1,21 @@
+﻿using MemberManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MemberManagementSystem.Controllers
+{
+    public class MemberController : Controller
+    {
+        MemberManagementSystemEntities db = new MemberManagementSystemEntities();
+
+        public ActionResult Index()
+        {
+            var data = db.Member.ToList();
+
+            return View(data);
+        }
+    }
+}
